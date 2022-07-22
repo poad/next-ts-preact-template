3b1da978f824d2ed9b629d3d4404d653630700c4
Kenji Saito <ken-yo@mbr.nifty.com>
2022-07-23 05:19:10 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
-import withPlugins from 'next-compose-plugins';
-import withPreact from 'next-plugin-preact';
-import withBundleAnalyzer from '@next/bundle-analyzer';
+const withPreact = require('next-plugin-preact');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 
-const analyzer = withBundleAnalyzer({
+const config = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-});
-const config = withPlugins([
-  [analyzer, withPreact],
-],
+})(withPreact(
   {
-    webpack5: true,
     reactStrictMode: true,
-    esmExternals: true,
-    swcLoader: true,
     swcMinify: true,
-    experimental: {
-      modern: true,
-    },
-  });
+  }));
 
-export default config;
+module.exports = config;
